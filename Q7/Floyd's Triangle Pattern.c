1e9b71db840255f3b9c5f9315a19a13740b2dc8a
ahmed <ahmedmohamedomar74@gmail.com>
2022-12-07 10:16:25 -0800

    Solve Q7

---
+++ Q7/Floyd's Triangle Pattern.c
@@ -0,0 +1,62 @@
+/**
+******************************************************
+*@file:Floyd's Triangle Pattern.c
+*@version:1.0.0
+*@author:Ahmed Mohamed Omar
+*@brief:tasks of Loop and coditions
+******************************************************
+*/
+/* ***************includes section start************* */
+#include<stdio.h>
+/* ***************Includes section end**************** */
+
+/* *************Definitions section start************* */
+/* *************Definitions section end*************** */
+
+/* *************Macros section srart*******************/
+/* *************Macros section end*********************/
+
+/* *************Global variables section srart******** */
+    int Loop_Iterator_One;
+    int Loop_Iterator_Two;
+    int rows;
+    int count=1;
+/* *************Global variables section end********** */
+
+/* *************Global decelartions section srart****** */
+/* *************Global decelartions section end******** */
+
+
+
+
+int main()
+{
+    printf("enter nubmbers of rows");
+    scanf("%i",&rows);
+    for (Loop_Iterator_One=1;Loop_Iterator_One<=rows;Loop_Iterator_One++)
+    {
+        for (Loop_Iterator_Two=1;Loop_Iterator_Two<=Loop_Iterator_One;Loop_Iterator_Two++)
+        {
+            if (Loop_Iterator_Two<Loop_Iterator_One)
+            {
+                printf("%i ",count++);
+            }
+            else
+            {
+                printf("%d ",count++);
+            }
+        }
+        printf("\n");
+    }
+}
+/* *************Sub program section srart****** */
+/* *************Sub program section end******** */
+
+
+/**
+***********************************************************
+user                    date            brief
+************************************************************
+Ahmed Mohamed Omar   12/7/2022 tasks of Loop and coditions
+
+*/
