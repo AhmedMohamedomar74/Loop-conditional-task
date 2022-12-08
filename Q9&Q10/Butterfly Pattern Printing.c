1029581eccf6294f09c868ccb2384309a8fa32df
ahmed <ahmedmohamedomar74@gmail.com>
2022-12-08 05:20:31 -0800

    Solve Q9&Q10

---
+++ Q9&Q10/Butterfly Pattern Printing.c
@@ -0,0 +1,48 @@
+/**
+******************************************************
+*@file:Hollow Diamond Inscribed in a Rectangle.c
+*@version:1.0.0
+*@author:Ahmed Mohamed Omar
+*@brief:tasks of Loop and coditions
+******************************************************
+*/
+/* ***************includes section start************* */
+#include<stdio.h>
+#include "Hollow Diamond Inscribed in a Rectangle.h"
+/* ***************Includes section end**************** */
+
+/* *************Definitions section start************* */
+/* *************Definitions section end*************** */
+
+/* *************Macros section srart*******************/
+/* *************Macros section end*********************/
+
+/* *************Global variables section srart******** */
+
+/* *************Global variables section end********** */
+
+/* *************Global decelartions section srart****** */
+
+/* *************Global decelartions section end******** */
+
+
+
+
+int main()
+{
+    printf("Enter the number of the rows");
+    scanf("%i",&rows);
+    Second_Part(rows);
+    First_Part(rows);
+}
+/* *************Sub program section srart****** */
+/* *************Sub program section end******** */
+
+
+/**
+***********************************************************
+user                    date            brief
+************************************************************
+Ahmed Mohamed Omar   12/8/2022 tasks of Loop and coditions
+
+*/
